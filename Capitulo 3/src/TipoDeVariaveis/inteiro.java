61b746b4db64eec2144818ba03c67abf7c9abb24
Unknown <davisouzamachado@outlook.com>
2017-12-19 21:45:19 -0200

    Capitulo 3

---
+++ Capitulo 3/src/TipoDeVariaveis/inteiro.java
@@ -0,0 +1,16 @@
+package TipoDeVariaveis;
+
+public class inteiro {
+	public static void main (String[] args) {		
+		// imprime a idade
+		int idade = 20;
+		System.out.println(idade);
+		
+		// gera a idade no ano seguinte
+		int idadeNoAnoQueVem;
+		idadeNoAnoQueVem = idade + 1;
+		
+		// imprime a idade
+		System.out.println(idadeNoAnoQueVem);
+	}
+}
