569eaa94ad18ce27a4447d1c8bd65c1e1e5ceec8
Unknown <davisouzamachado@outlook.com>
2018-01-03 21:24:01 -0200

    Exercicio2.java

---
+++ Capitulo 3/src/Exercicio/exercicio2.java
@@ -0,0 +1,12 @@
+package Exercicio;
+
+public class exercicio2 {
+
+	public static void main(String[] args) {
+		// Imprima a soma de 1 até 1000
+		for (int i = 1; i <= 1000; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
