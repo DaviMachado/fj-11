ada111a0ab27a96100f465ebcd1ad2f0b31cc37a
Unknown <davisouzamachado@outlook.com>
2018-01-06 18:13:24 -0200

    Exercicio4.java

---
+++ Capitulo 3/src/Exercicio/exercicio4.java
@@ -0,0 +1,12 @@
+package Exercicio;
+
+public class exercicio4 {
+	public static void main(String[] args) {
+		// Imprima os fatoriais de 1 a 10
+		int fatorial = 1;
+		for (int n = 1; n <= 10; n++) {
+			fatorial = fatorial * n;
+		}
+		System.out.println(fatorial);
+	}
+}
